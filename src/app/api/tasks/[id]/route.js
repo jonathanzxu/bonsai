6a0cefbd0754b5e17b58850b15fcc190a2c8b786
andrew-w-u <andrewwu7@g.ucla.edu>
2024-03-11 08:53:31 -0700

    updated task APIs based on existing task schema

--- src/app/api/tasks/[id]/route.js
+++ src/app/api/tasks/[id]/route.js
@@ -1,17 +1,18 @@
-import connectMongoDB from "../../../../libs/mongodb";
-import Task from "../../../../models/task";
+import connectDb from "../../../lib/database";
+import Task from "../../../lib/models/User";
+import {NextResponse} from "next/server";
 
 export async function PUT(request, {params}) {
     const { id } = params;
-    const {newName: name, newDescription: description, newPriority: priority} = await request.json();
-    await connectMongoDB();
-    await Task.findByIdAndUpdate(id, {name, description, prioirty});
-    return NextResponse.json({ message:: "Task successfully updated."}, { status: 200 });
+    const {newTitle: title, newDescription: description, newStatus: status, newUsers: users, newProject: project, newSubtasks: subtasks, newParent: parent, newdueDate: dueDate, newPriority: priority} = await request.json();
+    await connectDb();
+    await Task.findByIdAndUpdate(id, {title, description, status, users, project, subtasks, parent, dueDate, priority});
+    return NextResponse.json({ message: "Task successfully updated."}, { status: 200 });
 }
 
 export async function GET(request, { params }) {
     const { id } = params; 
-    await connectMongoDB();
-    const topic = await Topic.findOne({_id: id});
-    return NextResponse.json({ task }, {status: 200});
+    await connectDb();
+    const topic = await Task.findOne({_id: id});
+    return NextResponse.json({ Task }, {status: 200});
 }
