b15491b12fbbb17b34d60e6da4344a4415fe2d20
andrew-w-u <andrewwu7@g.ucla.edu>
2024-03-13 08:11:40 -0700

    fixed minor issue with params

--- src/app/api/tasks/[id]/route.tsx
+++ src/app/api/tasks/[id]/route.tsx
@@ -6,7 +6,7 @@ import connectDb from "../../../../lib/database";
 import Task from "../../../../lib/models/Task";
 import { NextResponse } from "next/server";
 
-export async function PUT(request: any, {params}) {
+export async function PUT(request: any, { params }: { params: { id: string } }) {
     const { id } = params;
     const {newTitle: title, newDescription: description, newStatus: status, newUsers: users, newProject: project, newSubtasks: subtasks, newParent: parent, newdueDate: dueDate, newPriority: priority} = await request.json();
     await connectDb();
@@ -14,7 +14,7 @@ export async function PUT(request: any, {params}) {
     return NextResponse.json({ message: "Task successfully updated."}, { status: 200 });
 }
 
-export async function GET(request: any, { params }) {
+export async function GET(request: any, { params }: { params: { id: string } }) {
     const { id } = params; 
     await connectDb();
     const task = await Task.findOne({_id: id});
