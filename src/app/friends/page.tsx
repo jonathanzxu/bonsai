a1a41a8ca2ff8384dbf8ddaefd9270f62aa96694
Chris Ro <chrisjoonrow@gmail.com>
2024-03-13 16:00:29 -0700

    Tried fixing some errors

--- src/app/friends/page.tsx
+++ src/app/friends/page.tsx
@@ -1,4 +1,8 @@
 "use client";
+import connectDb from "@/lib/database"
+import { getServerSession } from "next-auth"
+import { authOptions } from "@/app/api/auth/[...nextauth]/route"
+import User from "@/lib/models/User";
 import { Friends, columns } from "./columns";
 import { DataTable } from "./data-table";
 import { Input } from "@/components/ui/input";
@@ -6,7 +10,7 @@ import { Button } from "@/components/ui/button";
 import { useState } from "react";
 import { getFriends, addFriend } from "@/app/actions";
 
-function getData(): Friends[] {
+async function getData(): Promise<Friends[]> {
 
   // Fetch data from your API here.
     
@@ -46,26 +50,30 @@ function getData(): Friends[] {
 
 }
     
-export default function DemoPage() {
+export default async function DemoPage() {
   const data = getData();
   const [friendUsername, setFriendUsername] = useState("");
 
-  const trial = await myFunction();
+  const trial = await Function();
   //const trial = 'something';
 
 
   return (
     <>
-    <div className="container mx-auto py-10">
-      <DataTable columns={columns} data={data} />
-    </div>
-    <div className = "container flex-row w-full max-w-sm items-center space-x-2">
-
-    <div className="container flex w-full max-w-full space-x-2">
-      <Input className="flex-1" value={friendUsername} onChange={(e) => setFriendUsername(e.target.value)} placeholder="Username" />
-      <Button onClick={() => addFriend(friendUsername)}>Add</Button>
-    </div>
-    </div>
+      <div className="container mx-auto py-10">
+        <DataTable columns={columns} data={await getData()} />
+      </div>
+      <div className="container flex-row w-full max-w-sm items-center space-x-2">
+        <div className="container flex w-full max-w-full space-x-2">
+          <Input
+            className="flex-1"
+            value={friendUsername}
+            onChange={(e) => setFriendUsername(e.target.value)}
+            placeholder="Username"
+          />
+          <Button onClick={() => addFriend(friendUsername)}>Add</Button>
+        </div>
+      </div>
     </>
-  )
+  );
 }
